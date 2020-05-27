768b53cd35ab28806e253c6e9b04ed3147a2d04d
Cheikh Tidiane <kane.cheikhtidiane95@gmail.com>
2020-05-27 03:46:46 +0000

    merge

--- src/app/mot-passe-oublie/mot-passe-oublie.component.ts
+++ src/app/mot-passe-oublie/mot-passe-oublie.component.ts
@@ -8,13 +8,14 @@ import { LoginService } from '../services/login.service';
   selector: 'app-mot-passe-oublie',
   templateUrl: './mot-passe-oublie.component.html',
   styleUrls: ['./mot-passe-oublie.component.css']
-})   
+})
 export class MotPasseOublieComponent implements OnInit {
   motPassOublForm:FormGroup;
-  constructor(private fb:FormBuilder) { }   
+  isSend:boolean = false;
+  constructor(private fb:FormBuilder,private loginService:LoginService,private snackB: MatSnackBar) { }
 
   ngOnInit() {
-    this.initForm();     
+    this.initForm();
   }
   initForm(){
     this.motPassOublForm=this.fb.group({
@@ -23,4 +24,37 @@ export class MotPasseOublieComponent implements OnInit {
       newPassword:new FormControl('', Validators.required)
     })
   }
+
+
+
+  resetPassword(){
+    if(this.motPassOublForm.value['email'].indexOf("@") == -1 || this.motPassOublForm.value['email'].indexOf(".") == -1){
+      this.snackB.open("Veuillez renseigner une adresse e-mail valide","X", {
+        duration: 10000,
+        panelClass: ['my-snack-bar3','mat-success'],
+        verticalPosition: 'top',
+        horizontalPosition:'right',
+     });
+    }else{
+      
+      this.loginService.resetPasswordInit(this.motPassOublForm.value['email'])
+      .subscribe(
+        (data)=>{
+          console.log(data)
+          this.isSend = true;
+        },err=>{
+          console.log(err)
+          this.snackB.open("Une erreur s'est survenue, veuillez s'assurer que vous avez renseigner une adresse e-mail valide","X", {
+            duration: 10000,
+            panelClass: ['my-snack-bar3','mat-success'],
+            verticalPosition: 'top',
+            horizontalPosition:'right',
+         });
+        }
+      )
+    }
+  //  this.loginService.resetPasswordInit()
+   
+  }
 }
+
--- src/app/services/employe-exist.service.ts
+++ src/app/services/employe-exist.service.ts
@@ -11,8 +11,4 @@ export class EmployeExistService {
   getEmpExist(){
     return this.httpCl.get(environment.BASE_URL +'employeursByLogin');
   }
-
-  getEmployer(id){
-    return this.httpCl.get(environment.BASE_URL +'employeurs/'+id);
-  }
 }
